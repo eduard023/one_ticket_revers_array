a38599e022e83c5486dab4cf3f4a5e0140939b91
eduard023 <edo.avdzh@yandex.ru>
2022-10-09 01:07:18 +0300

    Перевернутый массив

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println("Изначальный массив ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("Перевернутый массив");
+        for (int i = arr.length-1; i != -1; i--) {
+            System.out.println(arr[i]);
+
+        }
+
     }
 }
